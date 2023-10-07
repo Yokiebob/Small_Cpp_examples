92342e80dd12c55657db1d23322a5201edd1da38
Yokiebob <126304146+Yokiebob@users.noreply.github.com>
2023-10-07 13:30:20 -0500

    comment numberDoubler.cpp

--- numberDoubler.cpp
+++ numberDoubler.cpp
@@ -2,9 +2,17 @@
 
 int main(){
 	int inputValue = 0;
-    std::cout << "Hello! Please input an integer.\n";
+	//we have to create a variable for cin to change I believe
+	
+	std::cout << "Hello! Please input an integer.\n";
+	//instructs user what to do
+	
 	std::cin >> inputValue;
+	//this will work for numbers that aren't too large.
+	
 	std::cout << "Your number doubled is " << inputValue*2 << "!\n\n";
+	//output the doubled number
+	
 	std::cout << "Not sure what else you want me to do.";
 	return 0;
 }
